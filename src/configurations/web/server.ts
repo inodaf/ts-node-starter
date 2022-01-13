2d28968ec34a422d6778303593d73d240776688f
Isac Fadoni <isacfadoni@gmail.com>
2022-01-13 13:45:51 -0300

    fix(orm): connect to DB only when needed

--- src/configurations/web/server.ts
+++ src/configurations/web/server.ts
@@ -5,7 +5,7 @@ export const app = Fastify({ logger: true });
 
 export const start = async () => {
   try {
-    await prisma.$connect();
+    // await prisma.$connect();
     await app.listen(3000);
   } catch (error: unknown) {
     app.log.error(error);

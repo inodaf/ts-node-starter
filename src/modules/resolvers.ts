239697168ad4e58fdf21e182e918783cad6a86db
Isac Fadoni <isacfadoni@gmail.com>
2022-04-06 17:54:51 -0300

    remove(mod/resolvers): unused resolver aggregator file

--- src/modules/resolvers.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-types */
-
-import { NonEmptyArray } from "type-graphql";
-import { GreetingsResolver } from "./hello/views/greetings.resolver";
-
-export const resolvers: NonEmptyArray<Function> | NonEmptyArray<string> = [
-  GreetingsResolver,
-];
